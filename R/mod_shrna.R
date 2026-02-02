b4e8d3f72e289893f5b8ab13e8986ef636915c5e
Hao He <haohe90@gmail.com>
2026-02-02 17:04:22 +0800

    add new dependency

--- R/mod_shrna.R
+++ R/mod_shrna.R
@@ -1,4 +1,11 @@
 # shinyBioTools - shRNA Module
+library(Biostrings)
+library(httr)
+library(xml2)
+library(AnnotationDbi)
+library(org.Hs.eg.db)
+library(org.Mm.eg.db)
+
 
 shRNAUI <- function(id) {
   ns <- NS(id)
